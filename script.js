2d5522a940a05655b576c9c1d4238a71db3dea3c
Siddhant sharma <hitechsidu992@gmail.com>
2023-01-23 16:54:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+function myFunction1() {
+
+    var x = document.getElementById("first-hide");
+	var l = document.getElementById("l1");
+	var m = document.getElementById("m1");
+    if (x.style.display != "none") {
+	  l.style.transform="rotate(90deg)";
+	  m.style.transform="rotate(0deg)";
+      x.style.display = "none";
+    } else {
+	  l.style.transform="rotate(50deg)";
+	  m.style.transform="rotate(-45deg)";
+      x.style.display = "block";
+    }
+  }
+  function myFunction2() {
+      var x = document.getElementById("secound-hide");
+	  var l = document.getElementById("l2");
+	  var m = document.getElementById("m2");
+      if (x.style.display != "none") {
+		l.style.transform="rotate(90deg)";
+	    m.style.transform="rotate(0deg)";
+        x.style.display = "none";
+      } else {
+		l.style.transform="rotate(50deg)";
+	    m.style.transform="rotate(-45deg)";
+        x.style.display = "block";
+      }
+    }
+    function myFunction3() {
+      var x = document.getElementById("third-hide");
+	  var l = document.getElementById("l3");
+	  var m = document.getElementById("m3");
+      if (x.style.display != "none") {
+		l.style.transform="rotate(90deg)";
+	    m.style.transform="rotate(0deg)";
+        x.style.display = "none";
+      } else {
+		l.style.transform="rotate(50deg)";
+	    m.style.transform="rotate(-45deg)";
+        x.style.display = "block";
+      }
+    }
+    function myFunction4() {
+        var x = document.getElementById("fourth-hide");
+		var l = document.getElementById("l4");
+	    var m = document.getElementById("m4");
+        if (x.style.display != "none") {
+		  l.style.transform="rotate(90deg)";
+	      m.style.transform="rotate(0deg)";
+          x.style.display = "none";
+        } else {
+		  l.style.transform="rotate(50deg)";
+	      m.style.transform="rotate(-45deg)";
+          x.style.display = "block";
+        }
+      }
+	  function myFunction5() {
+        var x = document.getElementById("fifth-hide");
+		var l = document.getElementById("l5");
+	    var m = document.getElementById("m5");
+        if (x.style.display != "none") {
+		  l.style.transform="rotate(90deg)";
+	      m.style.transform="rotate(0deg)";
+          x.style.display = "none";
+        } else {
+		  l.style.transform="rotate(50deg)";
+	      m.style.transform="rotate(-45deg)";
+          x.style.display = "block";
+        }
+      }
